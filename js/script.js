e12118b2000fd5ef5089931beac2933d38241c3c
Half-Demon-XD <152993761+Half-Demon-XD@users.noreply.github.com>
2026-02-19 21:59:37 +1100

    login fixes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("JS connected successfully");
+console.log("JS Loaded");
+
+// Wait until page loads
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginForm = document.getElementById("loginForm");
+
+    // Only run if we're on the login page
+    if (loginForm) {
+
+        loginForm.addEventListener("submit", function (event) {
+            event.preventDefault(); // stop page refresh
+
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            console.log("Login Attempt:");
+            console.log("Email:", email);
+            console.log("Password:", password);
+        });
+
+    }
+});
